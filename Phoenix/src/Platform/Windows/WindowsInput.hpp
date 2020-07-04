db327b49a5d774020657d31d21adb89b2bc684e4
Yash Pandey <62606998+EmperorYP7@users.noreply.github.com>
2020-07-04 23:14:30 +0530

    input polling

--- Phoenix/src/Platform/Windows/WindowsInput.hpp
+++ Phoenix/src/Platform/Windows/WindowsInput.hpp
@@ -1,1 +1,17 @@
 #pragma once
+
+#include "Phoenix/Input.hpp"
+
+namespace Phoenix
+{
+	class WindowsInput : public Input
+	{
+	protected:
+		virtual bool IsKeyPressedImpl(int keycode) override;
+
+		virtual bool IsMouseButtonPressedImpl(int button) override;
+		virtual std::pair<float, float> GetMousePositionImpl() override;
+		virtual float GetMouseXImpl() override;
+		virtual float GetMouseYImpl() override;
+	};
+}
--- Phoenix/src/Platform/Windows/WindowsWin.hpp
+++ Phoenix/src/Platform/Windows/WindowsWin.hpp
@@ -23,6 +23,7 @@ namespace Phoenix
 		}
 		void SetVSync(bool enabled) override;
 		bool IsVSync() const override;
+		inline virtual void* GetNativeWindow() const { return m_Window; }
 
 	private:
 		virtual void Init(const WindowProps& props);
